373bdbfecad24a43ed1dd0f7bf856b68937aab0b
JasonRothman <jrothman32@gmail.com>
2018-09-20 09:33:20 -0400

    Initial commit

--- src/Ch7Arrays/WeatherAnalysis.java
+++ src/Ch7Arrays/WeatherAnalysis.java
@@ -1,12 +1,17 @@
 package Ch7Arrays;
 
-import java.util.Arrays;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Scanner;
 
 public class WeatherAnalysis {
     public static void main(String[] args){
         //This program calculates the average temperature for N days
         // Identifies how many days are above the average
+        userInput();
+
+    }
+    public static void userInput(){
         //Step 1 : Ask how many days
         System.out.print("How many days' temperatures? ");
         Scanner sc = new Scanner(System.in);
@@ -23,8 +28,33 @@ public class WeatherAnalysis {
             sum +=x;
 
         }
-        System.out.println("Average temp = " + (double)sum/n);
+        double avg = (double)sum/n;
+        System.out.println("Average temp = " + round(avg));
+        aboveAverage(avg, numbers, n);
+
 
 
     }
+    public static double round(double value) {
+
+        BigDecimal bd = new BigDecimal(Double.toString(value));
+        bd = bd.setScale(1, RoundingMode.HALF_UP);
+        return bd.doubleValue();
+    }
+    public static void aboveAverage(double avg, int[] array, int n) {
+        int larger = 0;
+        for (int i = 1; i <= n; i++) {
+            if (array[i - 1] > avg) {
+                larger++;
+            }
+        }
+        if (larger > 1){
+            System.out.println(larger + " days were above average.");
+    }
+        else{
+            System.out.println(larger + " day were above average.");
+        }
+
+    }
 }
+
