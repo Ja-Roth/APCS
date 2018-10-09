9c8a70f930a756be66fb2aff67fa63d79d0d5199
Jason Rothman <jrothman32@gmail.com>
2018-10-09 16:53:57 -0400

    Initial commit

--- src/Ch7Arrays/JasonRothmanRandomHatV2.java
+++ src/Ch7Arrays/JasonRothmanRandomHatV2.java
@@ -1,38 +1,38 @@
 
 package Ch7Arrays;
 
-import java.util.Arrays;
+
 import java.util.Random;
 import java.util.Scanner;
 
 
 public class JasonRothmanRandomHatV2 {
     public static void main(String[] args) {
+        //create an empty array
         int[] array = {};
-        System.out.println("How many teams");
-
+        //user input
+        System.out.println("How many teams: ");
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        if (n%2!=0){
+        //system for checking if the number is odd or even than calls the method if it is even
+        if (n % 2 != 0) {
             System.out.println("Must be even number of teams");
-        }else{
-           array = RandomizeArray(1,n);
+        } else {
+            array = RandomizeArray(n);
+            divide(array);
         }
-        divide(array);
+    }
 
-
-
-        }
-    public static int[] RandomizeArray(int a, int b){
+    public static int[] RandomizeArray( int b) {
         Random rgen = new Random();  // Random number generator
-        int size = b-a+1;
+        int size = b ;
         int[] array = new int[size];
-
-        for(int i=0; i< size; i++){
-            array[i] = a+i;
+        //for loop to populate the array
+        for (int i = 0; i < size; i++) {
+            array[i] = 1 + i;
         }
-
-        for (int i=0; i<array.length; i++) {
+        // for loop to randomize the array
+        for (int i = 0; i < array.length; i++) {
             int randomPosition = rgen.nextInt(array.length);
             int temp = array[i];
             array[i] = array[randomPosition];
@@ -43,9 +43,10 @@ public class JasonRothmanRandomHatV2 {
         return array;
 
     }
-    public static void divide (int[] array){
-        for (int i =0;i<=array.length;i+=2){
-        System.out.println("Team" +array[i] +"Vs"+"Team" +array[i-1]);
+    // method to put the teams in an team vs team
+    public static void divide(int[] array) {
+        for (int i = 0; i <= array.length - 1; i += 2) {
+            System.out.println("Team " + array[i] + " Vs " + "Team " + array[i + 1]);
         }
     }
-    }
+}
