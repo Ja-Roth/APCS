874682d69beb7772e4d93e8108e67e9912b6bc55
Jason Rothman <jrothman32@gmail.com>
2018-11-27 09:47:41 -0500

    Initial commit

--- src/Ch8Classes/PointV1.java
+++ src/Ch8Classes/PointV1.java
@@ -1,4 +1,16 @@
+//This class is a template to represent a point
+//on the cartesian coordinate
 package Ch8Classes;
-
 public class PointV1 {
+    //state fields below (Variables)
+    public int x;
+    public int y;
+
+    //Behaviors below (Methods)
+    public void translate(int x, int y){
+
+    }
+    public void setLocation(int x, int y){
+
+    }
 }
