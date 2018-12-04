e59e559e6912732ff5cc0f5f0dad5b53ac8e35bd
Jason Rothman <jrothman32@gmail.com>
2018-12-04 09:18:31 -0500

    Initial commit

---
+++ src/Ch8Classes/PointClientV3.java
@@ -0,0 +1,13 @@
+package Ch8Classes;
+
+public class PointClientV3 {
+    public static void main(String[] args) {
+        //instantiate a point object called p1
+        PointV2 p1 = new PointV2();
+        p1.x = 3;
+        p1.y= 6;
+        System.out.println("P1: " + p1);
+
+        //PointV1 p1 = new PointV1 (3,6
+    }
+}
